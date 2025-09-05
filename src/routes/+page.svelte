1d270938ac18d9ea30e0b3053c3e758a5f8482ba
Derrick Holleman <d.holleman@fetchrewards.com>
2025-09-05 15:21:38 -0500

    remove test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script lang="ts">
-	const test3: number = 123;
-	console.log(test3);
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
